61aefcb6f3d924b5fd0508d5ae69ab69761c9c2c
ymus15 <kiebabkiebabmalakiebabownia@outlook.com>
2025-06-03 19:44:10 +0200

    Updated compiler.py

--- oOoOoOo/compiler.py
+++ oOoOoOo/compiler.py
@@ -10,12 +10,10 @@ def compile_oOoOoOo_to_python(input_file):
     with open(input_file, 'r') as f:
         code = f.read()
 
-    # Sprawdzenie końcówki pliku (10 pustych linii)
     if not code.endswith('\n' * 10):
         print("❌ Error!")
         return
-
-    # Dopasowanie składni tworzenia zmiennej
+        
     var_match = re.search(r"~'o (\w) :\+=\+> \{\>\[\^\(.../\"(.+)\"\\\.\.\.\)\^\]\<\}", code)
     if not var_match:
         print("❌ Error!")
@@ -24,13 +22,11 @@ def compile_oOoOoOo_to_python(input_file):
     var_name = var_match.group(1)
     var_value = var_match.group(2)
 
-    # Dopasowanie składni wypisania zmiennej
     print_match = re.search(r"~~'oa :\+=\+> ;;;\+=-\{" + re.escape(var_name) + r"\}-=\+;;;", code)
     if not print_match:
         print(f"❌ Error!")
         return
 
-    # Generowanie kodu Python
     python_code = f"{var_name} = '{var_value}'\n"
     python_code += f"oa = {var_name}\n"
     python_code += "print(oa)\n"
@@ -40,7 +36,6 @@ def compile_oOoOoOo_to_python(input_file):
 
     print("✅ Compiled")
 
-# Obsługa z terminala
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: python compiler.py <file.oOoOoOo>")
